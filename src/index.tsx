2038b61788ed80b1ad27505e4b19eceb003bebc7
Angelica Bayer <angelicacamboim@gmail.com>
2021-07-27 15:27:08 -0300

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/css/style.css'
-import './assets/css/font-awesome.min.css'
-import './assets/css/nice-select.css'
-import './assets/css/slicknav.min.css'
 
 ReactDOM.render(
 	<React.StrictMode>
